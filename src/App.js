e87ebd21abff59c94ddb8b89f5ea598948cb593e
Ezure-HARUNA <burimofumofu@gmail.com>
2021-06-30 15:42:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Auth from "./components/Auth";
+import Feed from "./components/Feed";
 
 const App = () => {
-  return <div>Toolkit</div>;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+  return (
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  );
 };
 
 export default App;
